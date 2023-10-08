619275300e32ecf9eb0042341a2f6ca6381c7a2b
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-10-08 10:12:44 +0300

    Update .condarc Add .config/fish/completions/pdm.fish Update .config/fish/functions/up.fish

--- dot_config/private_fish/functions/up.fish
+++ dot_config/private_fish/functions/up.fish
@@ -1,4 +1,8 @@
-function up --wraps='sudo dnf makecache --refresh && sudo dnf upgrade --refresh -y && flatpak update -y' --description 'alias up sudo dnf makecache --refresh && sudo dnf upgrade --refresh -y && flatpak update -y'
-  sudo dnf makecache --refresh && sudo dnf upgrade --refresh -y && flatpak update -y $argv
+function up --description 'update packages'
+  sudo dnf makecache --refresh && \
+  sudo dnf upgrade --refresh -y && \
+  flatpak update -y && \
+  chezmoi upgrade && \
+  pdm self update $argv
         
 end
