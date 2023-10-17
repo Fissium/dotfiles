c51546ed138b50d5ccd2cabc7f6f7fb5dc530ef2
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-10-17 12:50:40 +0300

    Remove mini.indetscope

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -1,13 +1,13 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	-- Add the community repository of plugin specifications
+	"AstroNvim/astrocommunity",
+	-- example of imporing a plugin, comment out to use it or add your own
+	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.motion.leap-nvim" },
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.syntax.hlargs-nvim" },
-  { import = "astrocommunity.editing-support.suda-vim" },
-  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
-  { import = "astrocommunity.completion.codeium-vim" },
+	{ import = "astrocommunity.motion.leap-nvim" },
+	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
+	{ import = "astrocommunity.syntax.hlargs-nvim" },
+	{ import = "astrocommunity.editing-support.suda-vim" },
+	{ import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+	{ import = "astrocommunity.completion.codeium-vim" },
 }
