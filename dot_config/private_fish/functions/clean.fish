80485b24da09da382e0db243266a38815cabb1ad
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-10-23 15:49:42 +0300

    Fix typo in clean.fish

--- dot_config/private_fish/functions/clean.fish
+++ dot_config/private_fish/functions/clean.fish
@@ -1,5 +1,5 @@
 function clean --description="clean system"
-    sudo dnf autoremove -y && \ 
-    dnf clean all && flatpak uninstall --unused -y && sudo journalctl --rotate --vacuum-size=100M && sh /home/fissium/tools/custom_sh/flatfolder.sh $argv
+    sudo dnf autoremove -y && dnf clean all && flatpak uninstall --unused -y && \
+    sudo journalctl --rotate --vacuum-size=100M && sh /home/fissium/tools/custom_sh/flatfolder.sh $argv
 
 end
