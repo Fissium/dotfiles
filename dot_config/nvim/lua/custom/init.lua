931254b572dccbf2e22ceeac82c4b5e5d97b2547
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-11-04 18:28:02 +0300

    Disable tmux when opening nvim

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,1 +1,10 @@
 vim.o.shell = "fish"
+
+-- disable tmux
+os.execute("tmux set-option -g status off")
+
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = function()
+		vim.fn.jobstart("tmux set-option -g status on", { detach = true })
+	end,
+})
