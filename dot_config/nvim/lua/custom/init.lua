c3300fbdfbf43684a399894e4f83b9551eaf375c
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-11-05 10:13:16 +0300

    Add tmux status bar on top

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,10 +1,1 @@
 vim.o.shell = "fish"
-
--- disable tmux
-os.execute("tmux set-option -g status off")
-
-vim.api.nvim_create_autocmd("VimLeave", {
-	callback = function()
-		vim.fn.jobstart("tmux set-option -g status on", { detach = true })
-	end,
-})
