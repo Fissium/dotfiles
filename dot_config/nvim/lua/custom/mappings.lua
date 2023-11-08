c30e773857ddfeb4e0e19df1d759edaf166405e8
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-11-08 16:29:54 +0300

    Remove ChatGPT due to quota limit via API

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -1,30 +1,6 @@
 ---@type MappingsTable
 local M = {}
 
-M.ChatGPT = {
-
-	v = {
-		["<leader>cg"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-		["<leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
-		["<leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
-		["<leader>ct"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
-		["<leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
-		["<leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
-		["<leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
-		["<leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
-	},
-	n = {
-		["<leader>cg"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-		["<leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
-		["<leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
-		["<leader>ct"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
-		["<leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
-		["<leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
-		["<leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
-		["<leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
-	},
-}
-
 M.neotest = {
 	n = {
 		["<leader>tt"] = {
--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -47,28 +47,6 @@ local plugins = {
 		end,
 	},
 
-	-- ChatGPT
-	{
-		"jackMort/ChatGPT.nvim",
-		cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTEditWithInstructions", "ChatGPTRun" },
-		config = function()
-			require("chatgpt").setup({
-				api_key_cmd = "gpg --decrypt " .. vim.fn.expand("$HOME") .. "/Documents/openai_api_key.txt.gpg",
-				chat = {
-					keymaps = {
-						scroll_down = "<C-d>",
-						draft_message = "<Nop>",
-					},
-				},
-			})
-		end,
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-	},
-
 	-- Codeium
 	{
 		"Exafunction/codeium.vim",
