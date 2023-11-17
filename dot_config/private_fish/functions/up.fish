929a38de44897f7892cceabb0b0760ad70db6d4c
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-11-17 16:49:15 +0300

    Remove .config/fish/functions/up.fish

--- dot_config/private_fish/functions/up.fish
+++
@@ -1,4 +0,0 @@
-function up --description 'update packages'
-    sudo dnf makecache --refresh && sudo dnf upgrade --refresh -y && flatpak update -y && chezmoi upgrade && pdm self update $argv
-
-end
