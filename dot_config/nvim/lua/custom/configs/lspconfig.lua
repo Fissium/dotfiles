f78d45287a85809ab50376eea0ce2e172b2bd8ac
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-12-24 12:10:50 +0300

    Fix typo

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -6,6 +6,7 @@ local lspconfig = require("lspconfig")
 -- if you just want default config for the servers then put them in a table
 local servers = {
 	"bashls",
+	"taplo",
 }
 
 for _, lsp in ipairs(servers) do
