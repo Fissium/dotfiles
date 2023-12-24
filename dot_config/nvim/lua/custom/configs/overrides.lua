38d24d33cdbe7bb197a48a5ea0d61b0e90b1aa27
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-12-24 11:57:03 +0300

    Update .config/nvim/lua/custom/configs/overrides.lua Change attributes of .config/fish/completions/chezmoi.fish Change attributes of .config/fish/completions/fisher.fish Change attributes of .config/fish/completions/pdm.fish Change attributes of .config/fish/config.fish Change attributes of .config/fish/functions/fisher.fish Change attributes of .config/fish/functions/lf.fish Change attributes of .config/fish/functions/lfcd.fish Change attributes of .config/fish/functions/ls.fish Change attributes of .config/fish/functions/lt.fish Change attributes of .config/fish/functions/mamba.fish Change attributes of .config/fish/functions/wg-down.fish Change attributes of .config/fish/functions/wg-up.fish

--- dot_config/nvim/lua/custom/configs/overrides.lua
+++ dot_config/nvim/lua/custom/configs/overrides.lua
@@ -1,68 +1,66 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "dockerfile",
-    "toml",
-    "yaml",
-    "json",
-    "jsonc",
-    "bash",
-    "fish",
-    "gitignore",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"dockerfile",
+		"toml",
+		"yaml",
+		"json",
+		"jsonc",
+		"bash",
+		"fish",
+		"gitignore",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- python stuff
-    "pyright",
-    "ruff-lsp",
+		-- python stuff
+		"pyright",
+		"ruff-lsp",
 
-    -- bash stuff
-    "bash-language-server",
-    "shellcheck",
-    "shfmt",
-    
-    -- yaml, markdown stuff
-    "yamlfmt",
-    -- docker stuff
-    "hadolint",
+		-- bash stuff
+		"bash-language-server",
+		"shellcheck",
+		"shfmt",
 
-    -- toml stuff
-    "taplo",
-  },
+		-- yaml stuff
+		"yamlfmt",
+
+		-- toml stuff
+		"taplo",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 return M
