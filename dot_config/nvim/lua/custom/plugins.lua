08ba4e95b69e761832f857c74828ff3e3d16775e
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2023-12-30 13:32:16 +0300

    Tweak starship, add the silent opt for the codeium commands

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -63,13 +63,13 @@ local plugins = {
 		config = function()
 			vim.keymap.set("i", "<C-g>", function()
 				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
+			end, { expr = true, silent = true })
 			vim.keymap.set("i", "<C-n>", function()
 				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
+			end, { expr = true, silent = true })
 			vim.keymap.set("i", "<C-p", function()
 				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true })
+			end, { expr = true, silent = true })
 		end,
 	},
 
