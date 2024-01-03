8ec1d1fa88907008aed36eb3d54ff31a32a67ff8
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-01-03 12:46:37 +0300

    Colorize up.fish

--- dot_config/fish/functions/up.fish
+++ dot_config/fish/functions/up.fish
@@ -1,18 +1,20 @@
 function up --description 'update the system'
   
-  echo "Updating packages..."
+  echo (set_color yellow) "Updating packages..."(set_color normal)
   sudo pacman -Syu
   sleep 1
-  echo "Done"
-  sleep 1
-  echo "Updating rustup and mamba..."
+  echo (set_color yellow) "Done"(set_color normal)
+  echo (set_color yellow) "Updating mamba..."(set_color normal)
   mamba self-update && mamba clean --all
   sleep 1
+  echo (set_color yellow) "Done"(set_color normal)
+  echo (set_color yellow) "Updating rustup..."(set_color normal)
   rustup self update && rustup update
-  echo "Done"
   sleep 1
-  echo "Removing rust-analyzer from $CARGO_HOME/bin..."
+  echo (set_color yellow) "Done"(set_color normal)
+  echo (set_color yellow) "Removing rust-analyzer from $CARGO_HOME/bin..."(set_color normal)
   rm -rf "$CARGO_HOME/bin/rust-analyzer"
-  echo "Done"
+  sleep 1
+  echo (set_color yellow) "Done"(set_color normal)
     
 end
