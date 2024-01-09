e3da78111b75227cb7ea2cce7048547e161970fc
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-01-09 12:02:21 +0300

    Refactor conform config

--- dot_config/nvim/lua/custom/configs/conform.lua
+++ dot_config/nvim/lua/custom/configs/conform.lua
@@ -1,21 +1,20 @@
-local options = {
+local M = {}
 
-	formatters_by_ft = {
-		-- lua
-		lua = { "stylua" },
-		-- python
-		-- use ruff_lsp for python
-		-- toml
-		toml = { "taplo" },
-		-- yaml
-		yaml = { "yamlfmt" },
-		-- shell
-		sh = { "shfmt" },
-		-- go
-    -- use gopls for golang
-		-- rust
-    -- use rust_analyzer for rust
-	},
+M.formatters_by_ft = {
+  -- lua
+  lua = { "stylua" },
+  -- python
+  -- use ruff_lsp for python
+  -- toml
+  -- use taplo for toml
+  -- yaml
+  yaml = { "yamlfmt" },
+  -- shell
+  sh = { "shfmt" },
+  -- go
+  -- use gopls for golang
+  -- rust
+  -- use rust_analyzer for rust
 }
 
-require("conform").setup(options)
+return M
