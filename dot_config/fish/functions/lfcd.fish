a38c0b9561f0e42d4805671d1682a560a67c18bc
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-04-01 18:57:11 +0300

    New lfcd.fish

--- dot_config/fish/functions/lfcd.fish
+++ dot_config/fish/functions/lfcd.fish
@@ -1,14 +1,5 @@
-function lfcd
-    set tmp (mktemp)
-    # `command` is needed in case `lfcd` is aliased to `lf`
-    command lf -print-last-dir=$tmp $argv
-    if test -f "$tmp"
-        set dir (cat $tmp)
-        rm -f $tmp
-        if test -d "$dir"
-            if test "$dir" != (pwd)
-                cd $dir
-            end
-        end
-    end
+function lfcd --wraps="lf" --description="lf - Terminal file manager (changing directory on exit)"
+    # `command` is needed in case `lfcd` is aliased to `lf`.
+    # Quotes will cause `cd` to not change directory if `lf` prints nothing to stdout due to an error.
+    cd "$(command lf -print-last-dir $argv)"
 end
