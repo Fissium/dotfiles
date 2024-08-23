df0cd84b246eb8b6b886716ff4df016187b881c0
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-08-23 15:07:47 +0300

    feat: https://github.com/NvChad/NvChad/issues/2938

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -3,12 +3,13 @@ local M = {}
 -- Path to overriding theme and highlights files
 local highlights = require("highlights")
 
-M.ui = {
-	theme = "onedark",
-	theme_toggle = { "onedark", "one_light" },
-
+M.base46 = {
+	theme = "chadracula",
 	hl_override = highlights.override,
 	hl_add = highlights.add,
+}
+
+M.ui = {
 
 	lsp = {
 		-- show function signatures i.e args as you type
