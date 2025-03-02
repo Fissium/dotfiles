2e3c97def7656495353fc8f811e52c90e73d2478
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2025-03-02 17:24:55 +0300

    feat: add crd for kyverno and argocd

--- dot_config/nvim/lua/configs/yaml-companion.lua
+++ dot_config/nvim/lua/configs/yaml-companion.lua
@@ -8,6 +8,22 @@ M.opts = {
 
 	schemas = {
 		{
+			name = "Kyverno Policy",
+			uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/kyverno.io/policy_v1.json",
+		},
+		{
+			name = "Kyverno Cluster Policy",
+			uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/kyverno.io/clusterpolicy_v1.json",
+		},
+		{
+			name = "Kyverno Policy Exception",
+			uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/kyverno.io/policyexception_v2.json",
+		},
+		{
+			name = "Argo CD Application",
+			uri = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json",
+		},
+		{
 			name = "Gitlab CI",
 			url = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json",
 		},
