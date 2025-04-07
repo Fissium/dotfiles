90a2c3a5917e4ed015bfa3c1eb5bb163b36ffff1
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2025-04-07 09:42:27 +0300

    fox: upd

--- dot_config/nvim/lua/configs/nvim-lint.lua
+++ dot_config/nvim/lua/configs/nvim-lint.lua
@@ -2,17 +2,27 @@ local M = {}
 
 M.linters_by_ft = {
 	-- docker
-	dockerfile = { "hadolint" },
+	dockerfile = { "hadolint", "typos" },
 	-- yaml
-	yaml = { "yamllint" },
+	yaml = { "yamllint", "typos" },
 	-- terraform
-	tf = { "tflint" },
-	terraform = { "tflint" },
-	["terraform-vars"] = { "tflint" },
+	tf = { "tflint", "typos" },
+	terraform = { "tflint", "typos" },
+	["terraform-vars"] = { "tflint", "typos" },
+	-- python
+	python = { "typos" },
+	-- bash
+	bash = { "typos" },
+	-- markdown
+	markdown = { "typos" },
+	-- helm
+	helm = { "typos" },
 	-- ansible
-	ansible = { "ansible_lint" },
-	-- all
-	["*"] = { "typos" },
+	ansible = { "ansible_lint", "typos" },
+	-- git
+	gitcommit = { "typos" },
+	-- lua
+	lua = { "typos" },
 }
 
 return M
