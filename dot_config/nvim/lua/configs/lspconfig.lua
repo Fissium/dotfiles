1f27082218bad8af5f4483efb487e7c47132edef
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2025-05-14 18:12:14 +0300

    feat: upd

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,87 +1,62 @@
-local nvlsp = require("nvchad.configs.lspconfig")
-local lspconfig = require("lspconfig")
-
-nvlsp.defaults()
+require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
-	bashls = {},
-	yamlls = {},
-	ruff = {},
-	helm_ls = {},
-	terraformls = {},
-	dockerls = {},
-	docker_compose_language_service = {},
-	marksman = {},
-	ansiblels = {},
-	pyright = {
-		settings = {
-			pyright = {
-				disableOrganizeImports = true,
+	"bashls",
+	"yamlls",
+	"ruff",
+	"ty",
+	"helm_ls",
+	"terraformls",
+	"dockerls",
+	"docker_compose_language_service",
+	"marksman",
+	"ansiblels",
+	"gopls",
+}
+
+-- gopls
+vim.lsp.config.gopls = {
+	settings = {
+		gopls = {
+			gofumpt = true,
+			codelenses = {
+				gc_details = false,
+				generate = true,
+				regenerate_cgo = true,
+				run_govulncheck = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = true,
 			},
-			python = {
-				analysis = {
-					autoSearchPaths = true,
-					diagnosticMode = "openFilesOnly",
-					useLibraryCodeForTypes = true,
-					typeCheckingMode = "basic",
-				},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
 			},
-		},
-	},
-	gopls = {
-		settings = {
-			gopls = {
-				gofumpt = true,
-				codelenses = {
-					gc_details = false,
-					generate = true,
-					regenerate_cgo = true,
-					run_govulncheck = true,
-					test = true,
-					tidy = true,
-					upgrade_dependency = true,
-					vendor = true,
-				},
-				hints = {
-					assignVariableTypes = true,
-					compositeLiteralFields = true,
-					compositeLiteralTypes = true,
-					constantValues = true,
-					functionTypeParameters = true,
-					parameterNames = true,
-					rangeVariableTypes = true,
-				},
-				analyses = {
-					nilness = true,
-					unusedparams = true,
-					unusedwrite = true,
-					useany = true,
-				},
-				usePlaceholders = true,
-				completeUnimported = true,
-				staticcheck = true,
-				directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-				semanticTokens = true,
+			analyses = {
+				nilness = true,
+				unusedparams = true,
+				unusedwrite = true,
+				useany = true,
 			},
+			usePlaceholders = true,
+			completeUnimported = true,
+			staticcheck = true,
+			directoryFilters = {
+				"-.git",
+				"-.vscode",
+				"-.idea",
+				"-.vscode-test",
+				"-node_modules",
+			},
+			semanticTokens = true,
 		},
 	},
 }
 
-for name, opts in pairs(servers) do
-	opts.on_init = nvlsp.on_init
-	opts.on_attach = nvlsp.on_attach
-
-	if name == "pyright" then
-		opts.capabilities = (function()
-			local capabilities = vim.deepcopy(nvlsp.capabilities)
-			capabilities.textDocument.publishDiagnostics = {
-				tagSupport = { valueSet = { 2 } },
-			}
-			return capabilities
-		end)()
-	else
-		opts.capabilities = nvlsp.capabilities
-	end
-
-	lspconfig[name].setup(opts)
-end
+vim.lsp.enable(servers)
